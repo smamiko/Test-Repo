bfa19b655af23d1c12bd8a287ede55e7a81c9ad4
Sato.M <s.mamiko@gmail.com>
2021-06-08 18:43:42 +0200

    Removeed some code

--- hello.py
+++ hello.py
@@ -1,16 +1,4 @@
-import sys
-from os import rename
-import math
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hej, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://asahi.com")
