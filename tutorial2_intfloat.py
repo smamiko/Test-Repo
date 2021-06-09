ebebb55bb1ac5b6f4ac3a4549a08fbc24b9efb3d
Sato.M <s.mamiko@gmail.com>
2021-06-09 09:42:33 +0200

    finished tutorial2_intfloat.py

---
+++ tutorial2_intfloat.py
@@ -0,0 +1,19 @@
+num_1 = 3
+num_2 = 2
+
+# num_1 += 5  # inclimentation of 5
+
+print(num_1)
+print(abs(-4))
+print(round(3.75))
+print(num_1 != num_2)
+
+num_3 = '100'
+num_4 = '200'
+
+print(num_3 + num_4)
+
+num_3 = int(num_3)
+num_4 = int(num_4)
+
+print(num_3 + num_4)
